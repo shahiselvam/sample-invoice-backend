57da0063ed47ff2e404e6b7646b9662b3298c7cb
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-17 14:00:36 +0530

    Update generatetoken.js

--- models/generatetoken.js
+++ models/generatetoken.js
@@ -7,7 +7,7 @@ const generateToken = (user , res) => {
     const expiration = process.env.DB_ENV === 'testing' ? 100 : 604800000;
 
     const token = jwt.sign({ _id:user._id , FirstName:user.FirstName , LastName:user.LastName , role:user.role, email:user.email} , process.env.TOKEN_SECRET, {
-        expiresIn: process.env.DB_ENV === 'testing' ? '1d' : '7d',
+        expiresIn: process.env.DB_ENV === 'production' ? '1d' : '7d',
       });
 
       return res.cookie('access_token' , token ,{
