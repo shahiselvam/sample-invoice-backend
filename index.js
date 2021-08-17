5dda5da5325a79914df3ee01d058e74b3e7127c8
shahiselvam <50788826+shahiselvam@users.noreply.github.com>
2021-08-17 13:41:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ async function loadApp(){
 
 mongo.connect();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
   })
 app.use(cors({
     origin: [
